adfd6dc27cc98e4c31585a2bf75d05c7338bb618
Abinesh-A <2001mrabi@gmail.com>
2023-02-28 17:00:01 +0530

    /..

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ export default function Home() {
 
   function someRequest() {
     //Simulates a request; makes a "promise" that'll run for 2.5 seconds
-    return new Promise((resolve) => setTimeout(() => resolve(), 2500));
+    return new Promise((resolve) => setTimeout(() => resolve(), 3000));
   }
 
   useEffect(() => {
