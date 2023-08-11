86bab08923ae6f93dc3a4a0baa9fb0bbf3972f04
Abinesh-A <2001mrabi@gmail.com>
2023-08-11 18:04:40 +0530

    about_me

--- src/pages/components/About.js
+++ src/pages/components/About.js
@@ -21,7 +21,7 @@ const About = () => {
               <span className={styles.about_tag}>#coding</span>
               <span className={styles.about_tag}> #dreams</span>
             </div>
-            <div className={styles.about_text}>FCINQ [ ɛfsɛ̃k ] n. 1. This is the key that refreshes the screen. 2. Originally graphic designers, animators and developers, today it is a team of around thirty people, whose jobs range from strategy to development, including brand content and ux design . Whatever the medium, they strive to put maximum quality at the service of creating experiences and brand universes in order to convey a feeling, an emotion and make your communication a successful exchange with the user. final. 3. Our projects say more about us than long speeches.</div>
+            <div className={styles.about_text}>"Experienced Web Developer and Software Engineer with a strong background in crafting innovative digital solutions. Proficient in both Web and Database technologies, I'm a maestro of crafting seamless user experiences through intuitive designs and responsive layouts. My expertise spans front-end and back-end development, and I thrive in collaborative, cross-functional teams. Committed to delivering clean, efficient code while staying updated with the latest industry trends, I'm ready to transform ideas into impactful online realities."</div>
           </div>
           <div className={styles.about_box_2}>
             <div>Code</div>
