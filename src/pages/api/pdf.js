4ed57462d62d7327b6b1125cfe723adb0c68fc1c
Abinesh-A <2001mrabi@gmail.com>
2023-10-13 12:43:09 +0530

    pdf_title+change

--- src/pages/api/pdf.js
+++ src/pages/api/pdf.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 
 export default function handler(req, res) {
-  const filePath = path.join(process.cwd(), 'public', 'pdf', 'resume.pdf');
+  const filePath = path.join(process.cwd(), 'public', 'pdf', 'Abinesh_Resume.pdf');
   const fileStream = fs.createReadStream(filePath);
 
   res.setHeader('Content-Type', 'application/pdf');
